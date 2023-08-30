d1829c95120575ff06d725e19b2b1c1c6aa37be5
Abhishek Raut <102864290+abhishekraut1@users.noreply.github.com>
2023-08-30 23:09:45 +0530

    file edited.

--- src/components/News.js
+++ src/components/News.js
@@ -33,7 +33,7 @@ const News = (props)=>{
     useEffect(() => {
       document.title = `${capatalizeFirstLetter(props.category)} - NEWS365`;
       updateNews();
-    })
+    },[])
 
     const fetchMoreData = async ()=>{
         const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
